3421438893d537772475916bac271ca2498cda76
Somesh Patil <someshpatil31102005@gmail.com>
2026-05-12 00:29:46 +0530

    Add PHP script to display a list of fruits

---
+++ elements_in_array.php
@@ -0,0 +1,13 @@
+<?php
+
+$fruits = array("Apple", "Mango", "Pineapple", "Orange", "Watermelon", "Guava");
+
+echo "Fruits List";
+echo "<br><br>";
+
+foreach($fruits as $fruit){
+  echo $fruit;
+  echo "<br>";
+}
+
+?>
